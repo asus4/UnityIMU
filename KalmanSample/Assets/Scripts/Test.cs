d97ee10ff7d6bbec9886b590d8d84e470212ccdd
koki ibukuro <koki.ibukuro@gmail.com>
2013-01-11 17:17:24 +0900

    first commit

---
+++ KalmanSample/Assets/Scripts/Test.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Kalman;
+
+/// <summary>
+/// Test code
+/// </summary>
+public class Test : MonoBehaviour {
+	
+	[SerializeField]
+	Camera cam;
+	
+	[SerializeField]
+	Transform mouseCube;
+	
+	[SerializeField]
+	Transform filterdCube;
+	
+	IKalmanWrapper kalman;
+	
+	void Awake ()
+	{
+		//kalman = new MatrixKalmanWrapper ();
+		kalman = new SimpleKalmanWrapper ();
+	}
+		
+	void Start ()
+	{
+		cam = Camera.mainCamera;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Vector3
+		Vector3 mouse = Input.mousePosition;
+		mouse.z = 10;
+		mouseCube.transform.position = cam.ScreenToWorldPoint (mouse);
+		
+		filterdCube.transform.position = cam.ScreenToWorldPoint (kalman.Update (mouse));
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# IMU in Unity Project
+
+
+- KalmanSample
+	- forked from [https://github.com/justserega/kalman-examples](https://github.com/justserega/kalman-examples)
+	
