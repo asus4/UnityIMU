71b2f0d644c0d7d7a800fa1f1c61dc06ff4aee35
koki ibukuro <koki.ibukuro@gmail.com>
2013-01-24 16:31:14 +0900

    クォータニオンサンプルとしてコミット

--- ImuSample/Assets/Scripts/IMU/IMUObject.cs
+++ ImuSample/Assets/Scripts/IMU/IMUObject.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using Imu;
 
 public class IMUObject : MonoBehaviour {
 	#region privates
@@ -10,10 +9,6 @@ public class IMUObject : MonoBehaviour {
 	Quaternion initialRotation;
 	Quaternion gyroInitialRotation;
 
-	// position
-	DVector3 accele;
-	DVector3 initialPosition;
-	DVector3 totalMovement;
 	#endregion
 
 	#region life cycle
@@ -28,10 +23,6 @@ public class IMUObject : MonoBehaviour {
 		initialRotation = _transform.localRotation;
 		gyroInitialRotation = Input.gyro.attitude;
 		
-		initialPosition = new DVector3 (_transform.localPosition);
-		
-		// set compass
-		accele = new DVector3 ();
 	}
 
 	void Update ()
@@ -43,18 +34,7 @@ public class IMUObject : MonoBehaviour {
 		// rotation
 		Quaternion offsetRotation = Quaternion.Inverse (gyroInitialRotation) * Input.gyro.attitude;
 		offsetRotation = this.getEulerInverse (offsetRotation);
-		_transform.localRotation = initialRotation * offsetRotation;
-		
-		// position
-		accele = updateAcceleration (accele);
-		totalMovement += accele;
-		_transform.localPosition = (initialPosition + totalMovement).ToVector3 ();
-		
-		// reset flag
-		if (Input.touchCount > 0) {
-			_transform.localPosition = this.initialPosition.ToVector3 ();
-			totalMovement = new DVector3 (0, 0, 0);
-		}
+		_transform.localRotation = initialRotation * offsetRotation;		
 	}
 	#endregion
 	
@@ -82,31 +62,5 @@ public class IMUObject : MonoBehaviour {
 		return Quaternion.Euler (e);
 	}
 
-	DVector3 updateAcceleration (DVector3 acc)
-	{
-		/*
-		Vector3 acceleration = Vector3.zero;
-		float deltaTime = Time.deltaTime;
-		float accDelta = deltaTime / (float)Input.accelerationEvents.Length;
-		
-		foreach (AccelerationEvent accEvent in Input.accelerationEvents) {
-			//acceleration += accEvent.acceleration * accEvent.deltaTime;
-			//Debug.Log (string.Format ("gyro acc:{0} delataTime:{1}", accEvent.acceleration, accEvent.deltaTime));
-		}
-		*/
-		
-		DVector3 gravAcc = new DVector3 (Input.gyro.gravity);
-		gravAcc *= 0.98;
-		
-		DVector3 userAcc = new DVector3 (Input.acceleration) - gravAcc;
-		
-		userAcc *= Time.deltaTime;
-		acc += userAcc;
-		acc *= 0.98;
-		
-		//Debug.Log (string.Format ("acc unity:{0} my:{1} gravity:{2}", Input.gyro.userAcceleration, acceleration-Input.gyro.gravity, Input.gyro.gravity));
-		
-		return acc;
-	}
 	#endregion
 }
